81bc5ed883a8eedb41b26369eaf0fc02328dcc21
sanket <telisanket2002@gmail.com>
2023-06-05 19:59:47 +0530

    implemented the loop to keep the game running and also added break to break the loop when the user guess the right number

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ use std::cmp::Ordering;
 fn main() {
     println!("Hello, world!");
     println!("Welcome to the game");
+    let mut random_number = rand::thread_rng().gen_range(1,30);
+   loop{
     println!("Enter a number:");
 
     let mut guess = String::new();
@@ -12,13 +14,18 @@ fn main() {
 
     println!("You guessed: {}", guess);
 
-    let mut random_number = rand::thread_rng().gen_range(1,30);
+   
   
 
     match guess.cmp(&random_number) {
         Ordering::Less => println!("guess bit higher"),
-        Ordering::Equal=> println!("right guess"),
+        Ordering::Equal=> {
+
+            println!("right guess")
+            break;
+        },
         Ordering::Greater=> println!("guess bit lower"),
 
     }
+   }
 }
